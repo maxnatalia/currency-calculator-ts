55ed0b802eaf69d3abce15f655c43268d5e29487
maxnatalia <Nataliamazur1988@gmail.com>
2023-05-24 13:22:37 +0200

    added AboutPage with content and styles

---
+++ src/pages/AboutPage/index.tsx
@@ -0,0 +1,60 @@
+const AboutPage = () => {
+  return (
+    <section className="section">
+      <h3>About Author</h3>
+      <p className="section__paragraph">
+        Hello there! My name is Natalia, and I am truly passionate about
+        animals. In fact, I share my home with a delightful dog and a charming
+        trio of parrots, creating a lively and vibrant atmosphere. 🐶🦜🐦🐦
+      </p>
+      <p className="section__paragraph">
+        Apart from my love for animals, I have also discovered a deep
+        fascination for programming. It's a field that allows me to exercise my
+        creativity, problem-solving skills, and logical thinking, all while
+        exploring the endless possibilities of technology. Over time, I have
+        gained substantial knowledge in various programming techniques,
+        continuously honing my skills and staying up to date with the latest
+        advancements.
+      </p>
+      <p className="section__paragraph">
+        But my interests don't end there! When I'm not immersed in programming,
+        you'll find me nurturing my beloved collection of plants. There's
+        something incredibly gratifying about caring for these living organisms
+        and witnessing their growth and beauty firsthand. It brings me joy and
+        helps me find balance and tranquility in my daily life.
+      </p>
+      <p className="section__paragraph">
+        As I continue on my journey, I'm always eager to expand my knowledge
+        further and embrace new challenges. The world of programming is
+        ever-evolving, and I strive to stay at the forefront, continually
+        learning and mastering new technologies and methodologies.
+      </p>
+      <p className="section__paragraph">
+        First project was simple and includes HTML, CSS and JavaScript. This is
+        more extended version. It contains more advanced techniques learned by
+        months of study and practise.
+      </p>
+      <p className="section__paragraph">
+        🚀You can spot the first version in Vanilla JavaScript here 👉
+        <a
+          className="section__link"
+          href="https://maxnatalia.github.io/Currency-Calculator/"
+          target="_blank"
+          rel="noreferrer"
+          title="Open App in New Card"
+        >
+          Currency-Calculator in Vanilla JS
+        </a>
+      </p>
+      <p className="section__paragraph">
+        If you liked my work and projects,{" "}
+        <a className="section__link" href="mailto:nataliamazur1988@gmail.com">
+          I warmly invite you to get in touch
+        </a>
+        ✉️. Together, we can create something exceptional!🖥️🤯
+      </p>
+    </section>
+  );
+};
+
+export default AboutPage;
