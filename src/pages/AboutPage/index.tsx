4441f92a9235bc5b5ec5a42f78bb9775af7f48d0
maxnatalia <Nataliamazur1988@gmail.com>
2023-05-25 11:33:57 +0200

    refactoring AboutPage - added article

--- src/pages/AboutPage/index.tsx
+++ src/pages/AboutPage/index.tsx
@@ -1,13 +1,13 @@
 const AboutPage = () => {
   return (
-    <section className="section">
+    <article className="article">
       <h3>About Author</h3>
-      <p className="section__paragraph">
+      <p className="article__paragraph">
         Hello there! My name is Natalia, and I am truly passionate about
         animals. In fact, I share my home with a delightful dog and a charming
         trio of parrots, creating a lively and vibrant atmosphere. 🐶🦜🐦🐦
       </p>
-      <p className="section__paragraph">
+      <p className="article__paragraph">
         Apart from my love for animals, I have also discovered a deep
         fascination for programming. It's a field that allows me to exercise my
         creativity, problem-solving skills, and logical thinking, all while
@@ -16,28 +16,28 @@ const AboutPage = () => {
         continuously honing my skills and staying up to date with the latest
         advancements.
       </p>
-      <p className="section__paragraph">
+      <p className="article__paragraph">
         But my interests don't end there! When I'm not immersed in programming,
         you'll find me nurturing my beloved collection of plants. There's
         something incredibly gratifying about caring for these living organisms
         and witnessing their growth and beauty firsthand. It brings me joy and
         helps me find balance and tranquility in my daily life.
       </p>
-      <p className="section__paragraph">
+      <p className="article__paragraph">
         As I continue on my journey, I'm always eager to expand my knowledge
         further and embrace new challenges. The world of programming is
         ever-evolving, and I strive to stay at the forefront, continually
         learning and mastering new technologies and methodologies.
       </p>
-      <p className="section__paragraph">
+      <p className="article__paragraph">
         First project was simple and includes HTML, CSS and JavaScript. This is
         more extended version. It contains more advanced techniques learned by
         months of study and practise.
       </p>
-      <p className="section__paragraph">
+      <p className="article__paragraph">
         🚀You can spot the first version in Vanilla JavaScript here 👉
         <a
-          className="section__link"
+          className="article__link"
           href="https://maxnatalia.github.io/Currency-Calculator/"
           target="_blank"
           rel="noreferrer"
@@ -46,14 +46,14 @@ const AboutPage = () => {
           Currency-Calculator in Vanilla JS
         </a>
       </p>
-      <p className="section__paragraph">
+      <p className="article__paragraph">
         If you liked my work and projects,{" "}
-        <a className="section__link" href="mailto:nataliamazur1988@gmail.com">
+        <a className="article__link" href="mailto:nataliamazur1988@gmail.com">
           I warmly invite you to get in touch
         </a>
         ✉️. Together, we can create something exceptional!🖥️🤯
       </p>
-    </section>
+    </article>
   );
 };
 
