87b25713268c7c1e5f526fea8366d59c08df0f37
maxnatalia <Nataliamazur1988@gmail.com>
2023-05-26 11:43:38 +0200

    added new props is PreviousData

--- src/common/Loader/index.tsx
+++ src/common/Loader/index.tsx
@@ -1,15 +1,19 @@
 interface LoaderProps {
-  content: string;
+  content?: string;
+  title?: string;
+  isPreviousData?: boolean;
 }
 
-const Loader = ({ content }: LoaderProps) => {
+const Loader = ({ content, title, isPreviousData }: LoaderProps) => {
+  const loaderClassName = isPreviousData ? "loader__previous" : "loader";
+
   return (
-    <div className="loader">
+    <div className={loaderClassName}>
+      <div className="loader__img" />
       <div className="loader__wrapper">
-        <h3 className="loader__title">Loading...</h3>
-        <div className="loader__img" />
+        <h3 className="loader__title">{title}</h3>
+        <p className="loader__content">{content}</p>
       </div>
-      <p className="loader__content">{content}</p>
     </div>
   );
 };
