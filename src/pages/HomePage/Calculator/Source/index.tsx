23f68a7bba48a235ce0a2c358a421894ec994d2f
maxnatalia <Nataliamazur1988@gmail.com>
2023-05-26 11:42:50 +0200

    created new component Source

---
+++ src/pages/HomePage/Calculator/Source/index.tsx
@@ -0,0 +1,17 @@
+const Source = () => {
+  return (
+    <p className="source">
+      The currency exchange rates were fetched from{" "}
+      <a
+        className="source__link"
+        href="https://exchangerate.host/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        exchangerate.host
+      </a>
+    </p>
+  );
+};
+
+export default Source;
