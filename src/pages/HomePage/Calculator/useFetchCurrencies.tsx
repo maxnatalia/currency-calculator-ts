15a41db6bcf571860c8472a483e36c9c2e3194de
maxnatalia <Nataliamazur1988@gmail.com>
2023-05-27 07:37:28 +0200

    added custom hook useFetchCurrencies

--- src/pages/HomePage/Calculator/useFetchCurrencies.tsx
+++ src/pages/HomePage/Calculator/useFetchCurrencies.tsx
@@ -1,21 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { wait } from "./wait";
-
-interface FetchData {
+export interface FetchData {
   rates: {
     [key: string]: number;
   };
   date: string;
 }
 
-export const useFetchCurrencies = () => {
+export const useFetchCurrencies = (base: string) => {
   const fetchCurrencies = async () => {
-    await wait(500);
-    const response = await fetch(`https://api.exchangerate.host/latest`);
+    await wait(1000);
+    const response = await fetch(
+      `https://api.exchangerate.host/latest?base=${base}&`
+    );
     return (await response.json()) as FetchData;
   };
 
-  const { isLoading, error, data } = useQuery(["currencies"], fetchCurrencies);
+  const { isLoading, error, isPreviousData, data } = useQuery(
+    ["currencies", base],
+    fetchCurrencies
+  );
 
-  return { isLoading, error, data };
+  return { isLoading, error, isPreviousData, data };
 };
