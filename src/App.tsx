8d4bc3b7a311a783140926b45f8594337bd49999
maxnatalia <Nataliamazur1988@gmail.com>
2023-06-02 07:32:21 +0200

    created mains Routes and common components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./common/Navigation";
+import Footer from "./common/Footer";
+import MainContainer from "./common/MainContainer";
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Navigation />
+      <MainContainer>
+        <Routes>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </MainContainer>
+      <Footer />
+    </HashRouter>
   );
 }
 
