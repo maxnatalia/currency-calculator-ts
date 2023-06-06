d9a468ffecfc029627d039cab79582e0293d4a48
maxnatalia <Nataliamazur1988@gmail.com>
2023-06-06 08:46:08 +0200

    updated paths in App for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { toAbout, toError, toHome } from "./routes";
 import Navigation from "./common/Navigation";
 import Footer from "./common/Footer";
 import MainContainer from "./common/MainContainer";
@@ -6,20 +7,20 @@ import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
 import ErrorPage from "./pages/ErrorPage";
 
-function App() {
+const App = () => {
   return (
     <HashRouter>
       <Navigation />
       <MainContainer>
         <Routes>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<ErrorPage />} />
+          <Route path={toAbout()} element={<AboutPage />} />
+          <Route path={toHome()} element={<HomePage />} />
+          <Route path={toError()} element={<ErrorPage />} />
         </Routes>
       </MainContainer>
       <Footer />
     </HashRouter>
   );
-}
+};
 
 export default App;
