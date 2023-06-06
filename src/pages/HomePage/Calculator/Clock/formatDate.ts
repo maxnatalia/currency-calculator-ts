847caa9b954106ec0309c9ecb071af93c17cf4f2
maxnatalia <Nataliamazur1988@gmail.com>
2023-06-06 08:47:01 +0200

    created separated file for formatDate

---
+++ src/pages/HomePage/Calculator/Clock/formatDate.ts
@@ -0,0 +1,10 @@
+export const formatDate = (date: Date) => {
+  return date.toLocaleString(undefined, {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+};
